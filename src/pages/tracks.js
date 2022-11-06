0604c9d8042dc14541a3bb0f1920f0367f34f2e2
nextorm77 <sonsw77@gmail.com>
2022-11-06 14:25:37 +0900

    initialized

---
+++ src/pages/tracks.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Layout } from "../components";
+import { useQuery, gql } from "@apollo/client";
+import TrackCard from "../containers/track-card";
+
+/**
+ * Tracks Page is the Catstronauts home page.
+ * We display a grid of tracks fetched with useQuery with the TRACKS query
+ */
+const TRACKS = gql`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`;
+
+const Tracks = () => {
+  const { loading, error, data } = useQuery(TRACKS);
+
+  if (loading) return "Loading...";
+
+  if (error) return `Error! ${error.message}`;
+
+  //return <Layout grid>{JSON.stringify(data)}</Layout>;
+  return (
+    <Layout grid>
+      {data?.tracksForHome?.map((track) => (
+        <TrackCard key={track.id} track={track} />
+      ))}
+    </Layout>
+  );
+};
+
+export default Tracks;
